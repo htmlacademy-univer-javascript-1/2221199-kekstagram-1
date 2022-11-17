3dcb4ddaecce5b0f617f9bb9c2cccd2ac9fcfe97
Keks <keks@htmlacademy.ru>
2022-11-18 01:08:36 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/big-picture.js
+++ 10/js/big-picture.js
@@ -1,9 +1,24 @@
-import {isEscapeKey} from "./util.js";
+import {isEscapeKey} from './util.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCloseButton = bigPicture.querySelector('.big-picture__cancel');
 
+const closeBigPicture = () => {
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  bigPictureCloseButton.removeEventListener('click', closeBigPicture);
+  document.removeEventListener('keydown', onPopupEscKeydown);
+};
+
+const addModalCloseHandlers = () => {
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  bigPictureCloseButton.addEventListener('click', closeBigPicture);
+  document.addEventListener('keydown', onPopupEscKeydown);
+};
+
 const openBigPicture = (pictureData) => {
+  addModalCloseHandlers();
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureData.url;
   bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
   bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length.toString();
@@ -21,37 +36,15 @@ const openBigPicture = (pictureData) => {
         <p class="social__text">${commentData.message}</p>
     </li>`);
   });
-  bigPicture.classList.remove('hidden');
   bigPicture.querySelector('.social__comment-count').classList.add('hidden');
   bigPicture.querySelector('.comments-loader').classList.add('hidden');
-  document.body.classList.add('modal-open');
 };
 
-const onPopupEscKeydown = (evt) => {
+function onPopupEscKeydown (evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeBigPicture();
   }
-};
+}
 
-const closeBigPicture = () => {
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  bigPictureCloseButton.removeEventListener('click', closeBigPicture);
-  document.removeEventListener('keydown', onPopupEscKeydown);
-};
-
-const addModalCloseHandlers = () => {
-  bigPictureCloseButton.addEventListener('click', closeBigPicture);
-  document.addEventListener('keydown', onPopupEscKeydown);
-};
-
-const addThumbnailClickHandler = (thumbnail, pictureData) => {
-  thumbnail.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    openBigPicture(pictureData);
-    addModalCloseHandlers();
-  });
-};
-
-export {addThumbnailClickHandler};
+export {openBigPicture};
