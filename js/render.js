52c0b773e922b45544cfb537f4aac9aa8be58368
qqsh1ne <qqsh1neee@gmail.com>
2022-11-18 03:07:49 +0500

    Fixed adding listeners and lint errors
    Used delegation for adding listeners on thumbnails

--- js/render.js
+++ js/render.js
@@ -1,5 +1,5 @@
 import {createPictures} from './data.js';
-import {addThumbnailClickHandler} from './big-picture.js';
+import {openBigPicture} from './big-picture.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictureList = document.querySelector('.pictures');
@@ -8,12 +8,19 @@ const picturesData = createPictures();
 
 picturesData.forEach((pictureData) => {
   const picture = pictureTemplate.cloneNode(true);
-  picture.querySelector('.picture__img').src = pictureData.url;
+  const pictureImg = picture.querySelector('.picture__img');
+  pictureImg.src = pictureData.url;
+  pictureImg.dataset.pictureData = JSON.stringify(pictureData);
   picture.querySelector('.picture__comments').textContent = pictureData.comments.length.toString();
   picture.querySelector('.picture__likes').textContent = pictureData.likes;
-
-  addThumbnailClickHandler(picture, pictureData);
   pictureListFragment.appendChild(picture);
 });
 
 pictureList.appendChild(pictureListFragment);
+
+pictureList.addEventListener('click', (evt) => {
+  const target = evt.target;
+  if (target.nodeName === 'IMG') {
+    openBigPicture(JSON.parse(target.dataset.pictureData));
+  }
+});
